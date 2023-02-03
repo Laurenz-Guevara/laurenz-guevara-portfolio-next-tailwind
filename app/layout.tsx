8c0927ea03ae9fabe4614488f34423a833e66d8e
Laurenz-Guevara <laurenzguevara@outlook.com>
2023-02-03 00:50:39 +0000

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css';
+
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={roboto.className}>
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}

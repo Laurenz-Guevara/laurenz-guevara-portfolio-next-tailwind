60ea92ed8a895747cbcb08a29d43bef844aea9ae
Laurenz-Guevara <laurenzguevara@outlook.com>
2023-02-24 22:25:13 +0000

    Added to work experience section

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 'use client';
 
 import React from 'react';
@@ -32,26 +33,50 @@ function WorkExperience() {
                 <p className="text-lg">2 Years, 4 Months</p>
               </div>
             </div>
-            <ul className="max-w-screen-lg py-6 lg:text-xl">
-              <li className="list-inside list-disc text-xl font-bold">
-                Web Application Support Engineer
-              </li>
-              <li className="pl-[26px]">
-                Developed with React, HTML, CSS, and JavaScript to build and
-                maintain websites for over 12 different clients. Communicated
-                with stakeholders and client teams on a weekly basis. Performed
-                deployments, CMS changes and monitoring reports.
-              </li>
-              <li className="list-inside list-disc pt-4 text-xl font-bold">
-                Associate Engineer
-              </li>
-              <li className="pl-[26px]">
-                Engaging in hands-on delivery of digital solutions with
-                measurable value to clients, as part of an agile development
-                team. To help to deliver working software solutions that meet
-                the quality and productivity standards of MMT Digital teams.
-              </li>
-            </ul>
+            <div className="max-w-screen-lg py-3 lg:text-xl [&>h1]:pb-1 [&>h1]:pt-4 [&>h1]:text-xl [&>h1]:font-bold">
+              <h1 className="text-xl ">Web Application Support Engineer</h1>
+              <p>
+                Developed with React, HTML, CSS/SCSS, and JavaScript to build
+                and maintain websites for over 12 different clients.
+                Communicated with stakeholders and client teams on a weekly
+                basis. Performed deployments, CMS changes and monitoring
+                reports.
+              </p>
+              <h1>My Role at MMT:</h1>
+              <ul className="pl-6 [&>li]:list-disc [&>li]:pb-1">
+                <li>
+                  Built new accessable and mobile-friendly pages, forms and
+                  components on both React and .NET websites.
+                </li>
+                <li>
+                  Develop with RESTful API's and Content Management Systems such
+                  as Kentico and Kontent.
+                </li>
+                <li>
+                  Design and implement new features in accordance with clients
+                  both alone and with other developers in an agile environment.
+                </li>
+                <li>
+                  Collaborated with a UX designer and translated Adobe XD
+                  designs into working components.
+                </li>
+                <li>
+                  Deployed production ready environments with Azure Pipelines.
+                </li>
+                <li>
+                  Troubleshooting and resolved both Front-end and Back-end bugs
+                  within tight response deadlines.
+                </li>
+                <li>
+                  Performed critical incident management and provided monthly
+                  monitoring reports.
+                </li>
+                <li>
+                  Worked with tools such as Confluence, Kanban, Azure DevOps,
+                  Bitbucket, Jira, GitHub and Adobe Suite.
+                </li>
+              </ul>
+            </div>
             <a
               href="#splashpage"
               className="rounded-xl bg-palette-1000 px-5 py-1 text-lg font-[500] text-white hover:opacity-90"
